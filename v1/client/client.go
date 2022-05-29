e48b9ad05daa19d65127d87828aac391ef37052b
Shyam Baitmangalkar <sbaitmangalkar@zscaler.com>
2022-05-29 17:06:31 +0530

    Updating port

--- v1/client/client.go
+++ v1/client/client.go
@@ -14,7 +14,7 @@ import (
 var (
 	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddr         = flag.String("addr", "localhost:8980", "The server address in the format of host:port")
+	serverAddr         = flag.String("addr", "localhost:9090", "The server address in the format of host:port")
 	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
 )
 
